569307ea825dcf5417cb888b82fc80c3a8aa27db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-26 10:29:29 +0000

    Deploy daggerok/cassandra to github.com/daggerok/cassandra.git:gh-pages

--- build/jgiven-report/data/metaData.js
+++ build/jgiven-report/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Aug 26, 2018 10:04:22 AM","version":"0.15.3-22e6c60d89d9","title":"JGiven Report","data":[],"showThumbnails":true} );
+jgivenReport.setMetaData({"created":"Aug 26, 2018 10:29:02 AM","version":"0.15.3-22e6c60d89d9","title":"JGiven Report","data":[],"showThumbnails":true} );
